e22853e08e119bd4fc13322ba78545f732c1a62e
Myron Weber <myron@radshop.com>
2015-01-09 01:02:15 -0800

    added cd and fugitive

--- bundle/clones.sh
+++ bundle/clones.sh
@@ -1,6 +1,8 @@
+cd ~/.vim/bundle
 git clone git@github.com:tpope/vim-rails.git
 git clone git@github.com:tpope/vim-bundler.git
 git clone git@github.com:vimoutliner/vimoutliner.git
 git clone git@github.com:thoughtbot/vim-rspec.git
 git clone git@github.com:nicholaides/words-to-avoid.vim.git
 git clone git@github.com:vim-ruby/vim-ruby.git
+git clone git@github.com:tpope/vim-fugitive.git
