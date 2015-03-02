4aeacecf89be73299e1379ad5f899cb863a7a745
Myron Weber <myron@radshop.com>
2015-03-02 10:14:34 -0800

    update clones script

--- bundle/clones.sh
+++ bundle/clones.sh
@@ -1,8 +1,43 @@
 cd ~/.vim/bundle
-git clone git@github.com:tpope/vim-rails.git
-git clone git@github.com:tpope/vim-bundler.git
-git clone git@github.com:vimoutliner/vimoutliner.git
-git clone git@github.com:thoughtbot/vim-rspec.git
-git clone git@github.com:nicholaides/words-to-avoid.vim.git
-git clone git@github.com:vim-ruby/vim-ruby.git
-git clone git@github.com:tpope/vim-fugitive.git
+
+if [ -d "vim-rails/.git" ]; then
+  git -C vim-rails pull
+else
+  git clone git@github.com:tpope/vim-rails.git
+fi
+
+if [ -d "vim-bundler/.git" ]; then
+  git -C vim-bundler pull
+else
+  git clone git@github.com:tpope/vim-bundler.git
+fi
+
+if [ -d "vimoutliner/.git" ]; then
+  git -C vimoutliner pull
+else
+  git clone git@github.com:vimoutliner/vimoutliner.git
+fi
+
+if [ -d "vim-rspec/.git" ]; then
+  git -C vim-rspec pull
+else
+  git clone git@github.com:thoughtbot/vim-rspec.git
+fi
+
+if [ -d "words-to-avoid.vim/.git" ]; then
+  git -C words-to-avoid.vim pull
+else
+  git clone git@github.com:nicholaides/words-to-avoid.vim.git
+fi
+
+if [ -d "vim-ruby/.git" ]; then
+  git -C vim-ruby pull
+else
+  git clone git@github.com:vim-ruby/vim-ruby.git
+fi
+
+if [ -d "vim-fugitive/.git" ]; then
+  git -C vim-fugitive pull
+else
+  git clone git@github.com:tpope/vim-fugitive.git
+fi
