f1ff180a91880b0d6b5333a1846a101f8889581d
Myron Weber <myron@radshop.com>
2021-01-23 00:06:43 -0800

    fix scripts for vagrant

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-ln -s .vim/.vimrc ~/.vimrc
+ln -f -s .vim/.vimrc ~/.vimrc
 ~/.vim/clones.sh
-mkdir ~/.temp
-sudo chown $USER:$USER ~/.viminfo
+mkdir -p ~/.temp
 vim +PluginInstall +qall
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s .vim/.tmux.conf ~/.tmux.conf
+ln -f -s .vim/.tmux.conf ~/.tmux.conf
+tmux new-session -d -s new
 tmux source ~/.tmux.conf
