917528a177cf99eee25316c4493b1ae0f41bfa85
Myron Weber <myron@radshop.com>
2021-01-22 23:32:18 -0800

    fix scripts

--- clones.sh
+++ clones.sh
@@ -5,6 +5,6 @@ cd ~/.vim/bundle
 if [ -d "vundle/.git" ]; then
   git -C vundle pull
 else
-  git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  git clone https://github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
