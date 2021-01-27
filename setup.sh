14bd14dd026bc404d939977449fb7c6b6935a4ae
Myron Weber <myron@radshop.com>
2021-01-27 10:31:23 -0800

    tweak file paths

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-ln -f -s .vim/.vimrc ~/.vimrc
+ln -f -s ~/.vim/.vimrc ~/.vimrc
 ~/.vim/clones.sh
 mkdir -p ~/.temp
 vim +PluginInstall +qall
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -f -s .vim/.tmux.conf ~/.tmux.conf
+ln -f -s ~/.vim/.tmux.conf ~/.tmux.conf
 tmux new-session -d -s new
 tmux source ~/.tmux.conf
